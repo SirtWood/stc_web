1d6d643f339281a5b2c31443690d9dfad6d093b3
Steven Torres Calero <stclink@gmail.com>
2023-05-20 01:37:20 -0500

    Se realizan cambios en projects.css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './shared/menu/menu.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { MainComponent } from './main/main.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    FooterComponent,
+    HomeComponent,
+    MainComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule,
